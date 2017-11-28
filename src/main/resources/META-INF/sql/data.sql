8f1e8b53f797190150dc9db86f8b594fb06aee2c
PiotrKloda <piotrek.kloda@gmail.com>
2017-11-28 16:06:56 +0100

    update

--- src/main/resources/META-INF/sql/data.sql
+++ src/main/resources/META-INF/sql/data.sql
@@ -1,45 +1,40 @@
 
 
 
-INSERT INTO categories (id,name,description) VALUES (1,'Kuchnia','Elemnty wykończenia kuchni');
-INSERT INTO categories (id,name,description) VALUES (2,'Lazienka','Elemnty wykończenia lazienki');
-INSERT INTO categories (id,name,description) VALUES (3,'Panele','Drewniane Wykończenie posadzki');
-INSERT INTO categories (id,name,description) VALUES (4,'Płytki','Ceramiczne wykonńczenie posadzki');
-INSERT INTO categories (id,name,description) VALUES (5,'Kanapy','Urządzenia do siedzenia');
+INSERT INTO categories (id,name,description) VALUES (1,'Kitchen','Kitchen design items');
+INSERT INTO categories (id,name,description) VALUES (2,'Bathroom','Bathroom design items');
+INSERT INTO categories (id,name,description) VALUES (3,'Floor','Floor finishing material');
+INSERT INTO categories (id,name,description) VALUES (4,'Wall','Paints for walls');
+INSERT INTO categories (id,name,description) VALUES (5,'Decorations','Decorative items');
+INSERT INTO categories (id,name,description,parent_id) VALUES (6,'Shower Cabin','Vertical shower cabins',2);
+INSERT INTO categories (id,name,description,parent_id) VALUES (7,'Sinks','Bathroom sinks',2);
 
 
-INSERT INTO `products` (`id`,`description`,`linkToPhoto`,`name`,`price`,`quantityUnit`,`producent`,`category_id`) VALUES (1,'Wanna miedziana','src/main/photowannna.jpg','Wanna',23,'item','Wannowicz',1);
-INSERT INTO `products` (`id`,`description`,`linkToPhoto`,`name`,`price`,`quantityUnit`,`producent`,`category_id`) VALUES (2,'Zlew ceramiczny','src/main/photozlew.jpg','Zlew',40,'item','Zlewinski',2);
-INSERT INTO `products` (`id`,`description`,`linkToPhoto`,`name`,`price`,`quantityUnit`,`producent`,`category_id`) VALUES (3,'Kabina złota','src/main/kabinazłota.jpg','Kabina',1940,'item','Kabiniewski',3);
-INSERT INTO `products` (`id`,`description`,`linkToPhoto`,`name`,`price`,`quantityUnit`,`producent`,`category_id`) VALUES (4,'Kanapa Andrzej','src/main/kabinazłota.jpg','Kanapka',2500,'item','Kanapowski',2);
-INSERT INTO `products` (`id`,`description`,`linkToPhoto`,`name`,`price`,`quantityUnit`,`producent`,`category_id`) VALUES (5,'Farba brzydka','src/main/kabinazłota.jpg','Farba Paw',203,'m2/l','Farbowicz',1);
+INSERT INTO `products` (`id`,`description`,`linkToPhoto`,`name`,`price`,`quantityUnit`,`producent`,`category_id`) VALUES (1,'Silver shower cabin','src/main/photowannna.jpg','Shower cabin',23,'item','Showerer',6);
+INSERT INTO `products` (`id`,`description`,`linkToPhoto`,`name`,`price`,`quantityUnit`,`producent`,`category_id`) VALUES (2,'Ceramic sink','src/main/photozlew.jpg','Sink 20d',310,'item','Showerer',2);
+INSERT INTO `products` (`id`,`description`,`linkToPhoto`,`name`,`price`,`quantityUnit`,`producent`,`category_id`) VALUES (3,'Round mirror','src/main/kabinazłota.jpg','Roundie',1940,'item','MirrorMan',2);
+INSERT INTO `products` (`id`,`description`,`linkToPhoto`,`name`,`price`,`quantityUnit`,`producent`,`category_id`) VALUES (4,'Refrigerator BIG','src/main/kabinazłota.jpg','Fridgie',2500,'item','Cooler',1);
+INSERT INTO `products` (`id`,`description`,`linkToPhoto`,`name`,`price`,`quantityUnit`,`producent`,`category_id`) VALUES (5,'Wodden Oak panels','src/main/kabinazłota.jpg','Oak 2ES',45,'m2','SwissWood',3);
 
 
-INSERT INTO `attributes` (`id`,`name`,`unit`) VALUES (1,'Waga','kg');
-INSERT INTO `attributes` (`id`,`name`,`unit`) VALUES (2,'Długość','m');
-INSERT INTO `attributes` (`id`,`name`,`unit`) VALUES (3,'Objętość','m3');
-INSERT INTO `attributes` (`id`,`name`,`unit`) VALUES (4,"Grubość",'cm');
-INSERT INTO `attributes` (`id`,`name`,`unit`) VALUES (5,"Rozmiar",'m');
-INSERT INTO `attributes` (`id`,`name`,`unit`) VALUES (6,"pojemność",'cm3');
+INSERT INTO `attributes` (`id`,`name`,`unit`) VALUES (1,'Weight','kg');
+INSERT INTO `attributes` (`id`,`name`,`unit`) VALUES (2,'Length','m');
+INSERT INTO `attributes` (`id`,`name`,`unit`) VALUES (3,'Volume','m3');
+INSERT INTO `attributes` (`id`,`name`,`unit`) VALUES (4,'Size','cm');
+INSERT INTO `attributes` (`id`,`name`,`unit`) VALUES (5,'Material','-');
+INSERT INTO `attributes` (`id`,`name`,`unit`) VALUES (6,'Covering area','m2/l');
 
 
-
-
---INSERT INTO `categories_attributes` (`categories_id`,`attributes_id`) VALUES (1,1);
---INSERT INTO `categories_attributes` (`categories_id`,`attributes_id`) VALUES (1,2);
---INSERT INTO `categories_attributes` (`categories_id`,`attributes_id`) VALUES (1,3);
---INSERT INTO `categories_attributes` (`categories_id`,`attributes_id`) VALUES (2,1);
-
-INSERT INTO `attributes_categories` (`categories_id`,`attributes_id`) VALUES (1,1);
-INSERT INTO `attributes_categories` (`categories_id`,`attributes_id`) VALUES (1,2);
-INSERT INTO `attributes_categories` (`categories_id`,`attributes_id`) VALUES (1,3);
-INSERT INTO `attributes_categories` (`categories_id`,`attributes_id`) VALUES (2,3);
+INSERT INTO `attributes_categories` (`categories_id`,`attributes_id`) VALUES (6,1);
+INSERT INTO `attributes_categories` (`categories_id`,`attributes_id`) VALUES (6,2);
+INSERT INTO `attributes_categories` (`categories_id`,`attributes_id`) VALUES (6,3);
+INSERT INTO `attributes_categories` (`categories_id`,`attributes_id`) VALUES (6,5);
 INSERT INTO `attributes_categories` (`categories_id`,`attributes_id`) VALUES (2,2);
 INSERT INTO `attributes_categories` (`categories_id`,`attributes_id`) VALUES (3,1);
 INSERT INTO `attributes_categories` (`categories_id`,`attributes_id`) VALUES (4,4);
 
-INSERT INTO `prodAttrValues` (`id`,`value`,`attribute_id`,`product_id`) VALUES (1,'16',1,1);
-INSERT INTO `prodAttrValues` (`id`,`value`,`attribute_id`,`product_id`) VALUES (2,'1.3',2,4);
-INSERT INTO `prodAttrValues` (`id`,`value`,`attribute_id`,`product_id`) VALUES (3,'Tak',3,3);
-INSERT INTO `prodAttrValues` (`id`,`value`,`attribute_id`,`product_id`) VALUES (4,'43',4,1);
-INSERT INTO `prodAttrValues` (`id`,`value`,`attribute_id`,`product_id`) VALUES (5,'Dużo',2,2);
+INSERT INTO `prodAttrValues` (`id`,`value`,`attribute_id`,`product_id`) VALUES (1,'65',1,1);
+INSERT INTO `prodAttrValues` (`id`,`value`,`attribute_id`,`product_id`) VALUES (2,'1.2',2,1);
+INSERT INTO `prodAttrValues` (`id`,`value`,`attribute_id`,`product_id`) VALUES (3,'800',3,1);
+INSERT INTO `prodAttrValues` (`id`,`value`,`attribute_id`,`product_id`) VALUES (4,'Plexi',5,1);
+INSERT INTO `prodAttrValues` (`id`,`value`,`attribute_id`,`product_id`) VALUES (5,'0.8',2,2);
